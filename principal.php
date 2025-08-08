e65cab4a00a2bb4e0c6a1d5d318374f99975da91
Matheus <matheuscoelhohenrique7@gmail.com>
2025-08-08 17:04:19 -0300

    principal php
    parte php do principal feita

---
+++ principal.php
@@ -0,0 +1,35 @@
+<?php 
+session_start();
+require_once 'conexao.php';
+
+ if (!isset($_SESSION['usuario'])) {
+    header("location: login.php");
+    exit();
+ }
+// OBTENDO O NOME DO PERFIL DO USUARIO LOGADO
+$id_perfil = $_SESSION['perfil'];
+$sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
+$stmtPerfil = $pdo->prepare($sqlPerfil);
+$stmtPerfil->bindParam(':id_perfil', $id_perfil);
+$stmtPerfil->execute();
+$perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
+$nome_perfil = $perfil['nome_perfil'];
+
+// DEFINIÇÃO DAS PERMISSÕES POR PERFIL
+$permissoes = [
+    1 => ["Cadastrar"=>["cadastro_usuario.php", "cadastro_perfil.php", "cadastro_cliente.php","cadastro_fornecedor.php", "cadastro_produto.php", "cadastro_funcionario.php"]],
+    
+    "Buscar"=>["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
+    
+    "Alterar"=>["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
+    
+    "Excluir"=>["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"],
+]
+
+
+
+
+
+
+
+?>
